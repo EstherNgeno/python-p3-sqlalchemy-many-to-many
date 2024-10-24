2f68fea74004613f1721fff293e25c692e14a58a
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-10-24 19:25:21 +0300

    final commit

---
+++ lib/migrations/versions/c878b81c2eca_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c878b81c2eca
+Revises: a28af33e6866
+Create Date: 2024-10-24 18:30:37.114752
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c878b81c2eca'
+down_revision = 'a28af33e6866'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
